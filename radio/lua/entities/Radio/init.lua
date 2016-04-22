943d983abda78a42e32bb3db2a08dbd209e19f94
Matt Jeanes <mattjeanes23@gmail.com>
2016-04-22 21:04:14 +0100

    Added YouTube support
    Uses my API

--- radio/lua/entities/Radio/init.lua
+++ radio/lua/entities/Radio/init.lua
@@ -31,12 +31,55 @@ function ENT:Use( activator, caller )
 	net.Send(activator)
 end
 
-net.Receive("SendURL", function( len, ply)
-	local URL=net.ReadString()
+local patterns={
+	"youtu.be/([_A-Za-z0-9-]+)",
+	"youtube.com/watch%?v=([_A-Za-z0-9-]+)",
+}
+local api="https://abyss.mattjeanes.com:8090/"
+function ENT:ResolveMusicURL(url,callback)
+	if url:find("youtu.be") or url:find("youtube.com") then
+		local id=string.match(url,patterns[1]) or string.match(url,patterns[2])
+		if id then
+			http.Fetch(api.."get?id="..id,
+				function(body,len,headers,code)
+					local tbl=util.JSONToTable(body)
+					if tbl then
+						if tbl.success then
+							callback(null,(api.."play?id="..id))
+						else
+							callback("Failed to load ("..(tbl.err and tbl.err or "Unknown reason")..")")
+						end
+					else
+						callback("Failed to load API response")
+					end
+				end,
+				function(err)
+					callback("Failed to resolve url ("..err..")")
+				end
+			)
+		else
+			callback("Couldn't find video ID inside url")
+		end
+	else
+		callback(nil,url)
+	end
+end
+
+net.Receive("SendURL", function(len, ply)
+	local url=net.ReadString()
 	local ent=net.ReadEntity()
-		net.Start("BroadcastURL")
-			net.WriteString(URL)
-			net.WriteEntity(ent)
-		net.Broadcast()
+	if ent.ResolveMusicURL then
+		ent:ResolveMusicURL(url,function(err,url)
+			if err then
+				ply:ChatPrint("ERROR: "..err)
+			else
+				ply:ChatPrint("Playing!")
+				net.Start("BroadcastURL")
+					net.WriteString(url)
+					net.WriteEntity(ent)
+				net.Broadcast()
+			end
+		end)
+	end
 end)
 
